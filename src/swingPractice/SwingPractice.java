7e1d2aaaaf7f095f1d9d0e72d5cb757bf7ca8a03
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-11-10 12:10:31 +0100

    Init project, check compilation

--- src/swingPractice/SwingPractice.java
+++ src/swingPractice/SwingPractice.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package java.swing.practice;
+package swingPractice;
 
 /**
  *
  * @author Wu
  */
-public class JavaSwingPractice {
+public class SwingPractice {
 
     /**
      * @param args the command line arguments
