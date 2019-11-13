a4b42a723a524ba44bc30ff8ca238d4c5923b7a8
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-11-13 17:37:09 +0100

    Change frame definition to class extending JFrame. Center the frame

--- src/justFrame/Frame.java
+++ src/justFrame/Frame.java
@@ -1,26 +1,30 @@
 package justFrame;
 
 import javax.swing.*;
-import java.awt.*; // awt - abstract window toolkit
- 
-public class Frame 
+import java.awt.*; // awt - abstract window toolkit 
+
+
+public class Frame extends JFrame
 {    
     public Frame()
     {
-        JFrame frame = new JFrame("Hello");
-        //frame.setSize(100, 100);
-        //frame.setLocation(new Point (100, 50));
-        //frame.setResizeable(false); // default true
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
         
-        frame.setBounds(100, 50, 100, 100);
-        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.jpg"));
-        frame.setDefaultCloseOperation(3);
-        frame.setVisible(true);
+        this.setSize(width/2, height/2);
+        
+        int frameWidth = this.getSize().width;
+        int frameHeight = this.getSize().height;
+        
+        this.setLocation((width-frameWidth)/2, (height-frameHeight)/2);
+        
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.jpg"));
+        this.setDefaultCloseOperation(3);
     }
     
     public static void main(String[] args) 
     {
-        new Frame();
+        new Frame().setVisible(true);
         System.out.println("Hello");
     }
 }
